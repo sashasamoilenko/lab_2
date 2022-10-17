f43369167a5f1fbe243108a9d681d564bd2ef561
sasha <samsha250@gmail.com>
2022-10-17 19:58:57 +0300

    multiple mults

--- main.cpp
+++ main.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
 
-class Lint {
+class Multer {
+public:
+    virtual void multiply() = 0;
+};
 
+class Karatsuba : public Multer {
+public:
+     void multiply() {
+         //...
+     }
+};
+
+class Strassen : public Multer {
+public:
+    void multiply() {
+        //...
+    }
+};
+
+class Lint {
+private:
+    static Multer *multer;
+public:
+    static void setMultMode(Multer *newMulter) {
+       multer = newMulter;
+    }
+
+    Lint operator* (Lint other) {
+        multer->multiply();
+    }
 };
 int main() {
     Lint a = "837173432094389813435565409985", b, c, d, e;
     b = "19";
     std::cin >> c;
+
+    Lint::setMultMode(new Karatsuba());
 
     std::cout << a * (b + c) << std::endl;
 
