e1bd4139203cec9d89536052b785e466155ebc75
sasha <samsha250@gmail.com>
2022-10-17 18:32:33 +0300

    start

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
-int main (){
-    std::cout << "Hello world" << std::endl;
+
+class Lint {
+
+};
+int main() {
+    Lint a = "837173432094389813435565409985", b, c, d, e;
+    b = "19";
+    std::cin >> c;
+
+    std::cout << a * (b + c) << std::endl;
+
+    return 0;
+
 };
