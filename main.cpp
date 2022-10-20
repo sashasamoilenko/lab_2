80409c579cc3fa2df3edf2fc71115b3ee0c7466e
sasha <samsha250@gmail.com>
2022-10-20 21:07:28 +0300

    operator +

--- main.cpp
+++ main.cpp
@@ -29,16 +29,42 @@ public:
     }
 
     Lint(string digits_str) {
-        for(int i = 0; i < digits_str.size(); ++i){
+        for(int i = digits_str.size()-1; i >= 0 ; --i){
             digits.push_back(digits_str[i] - '0');
         }
+    }
+
+    Lint operator+(Lint other) {
+        Lint res;
+        int carry = 0;
+
+        int max_len = digits.size() > other.digits.size() ? digits.size() : other.digits.size();
+
+        for (int i = 0; i < max_len; ++i) {
+            int d = 0;
+            if (i < digits.size()) {
+                d += digits[i];
+            }
+
+            if (i < other.digits.size()) {
+                d += other.digits[i];
+            }
+
+            res.digits.push_back((d + carry) % 10);
+            carry = (d + carry) / 10;
+        }
+
+        if (carry > 0){
+            res.digits.push_back(carry);
+        }
+        return res;
     }
 //    static void setMultMode(Multer *newMulter) {
 //       multer = newMulter;
 //    }
 
     void print() {
-        for (int i = 0; i < digits.size(); ++i) {
+        for (int i = digits.size()-1; i >= 0 ; --i) {
             cout << digits[i];
         }
         cout << endl;
@@ -49,8 +75,10 @@ public:
     }
 };
 int main() {
-    Lint a("83717"), b, c, d, e;
+    Lint a("133"), b("1928"), c, d, e;
     a.print();
+    c = a+b;
+    c.print();
 //    b = "19";
 //    std::cin >> c;
 
